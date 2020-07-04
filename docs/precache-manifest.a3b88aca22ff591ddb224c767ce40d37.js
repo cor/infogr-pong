c286c64f09d1d86a44babedcaa041c28cb58b7c9
cor <cor@pruijs.nl>
2020-07-04 15:12:34 +0200

    Deploy

---
+++ docs/precache-manifest.a3b88aca22ff591ddb224c767ce40d37.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5085e11daa4d890913fbdb319257c7e",
+    "url": "/infogr-pong/404.html"
+  },
+  {
+    "revision": "9f1901e1321f79cee650",
+    "url": "/infogr-pong/css/app.9c03e382.css"
+  },
+  {
+    "revision": "4c1c3c9aa577140f2f7e8ae47d2a293f",
+    "url": "/infogr-pong/index.html"
+  },
+  {
+    "revision": "792387564e323932678d",
+    "url": "/infogr-pong/js/about.c058c680.js"
+  },
+  {
+    "revision": "9f1901e1321f79cee650",
+    "url": "/infogr-pong/js/app.fdfdab23.js"
+  },
+  {
+    "revision": "af44d2a93ec7d451b576",
+    "url": "/infogr-pong/js/chunk-vendors.7d9f1d29.js"
+  },
+  {
+    "revision": "124e9e43b4163fb40b51b6be966f8d17",
+    "url": "/infogr-pong/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/infogr-pong/robots.txt"
+  }
+]);
